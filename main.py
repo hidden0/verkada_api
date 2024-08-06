229f24d928e4148989885fc6baf0772705b36742
John Thorne <2028402+hidden0@users.noreply.github.com>
2024-08-06 17:47:07 -0600

    Initial commit - lots of work to do!

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 #imports
-from lib.vapi import vapi
+from datetime import timezone
 from pprint import pprint
+from lib.vapi import Vapi
+from datetime import datetime, timedelta
 
 def main():
-        print("Verkada API Framework")
+    # Initialize the Vapi instance
+    vapi = Vapi()
 
-        myAPI = vapi()
-        response = myAPI.sendRequest(product="core", endpoint="user", params=)
-        pprint(response.text)
+    # Define camera and organization IDs
+    org_id = "48684ea6-d592-436f-a282-5f6aad829d06"
+    camera_id = "a89a5995-5d3b-4b69-9ade-11c201952be0"
+
+    # Calculate the start and end times for the last hour
+    end_time = datetime.now(timezone.utc)
+    start_time = end_time - timedelta(minutes=5)
+
+    # Get historic footage
+    vapi.get_historic_footage(camera_id, org_id, start_time, end_time, "6")
+    #vapi.download_all_cameras(org_id, start_time, end_time)
 
 if __name__ == "__main__":
-	main()
+    main()
